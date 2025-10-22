3a45b76e22da6fd067dd1158eb6bb6a7aaac289a
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2025-10-21 20:58:15 -0500

    chore:adding nofification route

--- test/routesApi_public_test.sh
+++ test/routesApi_public_test.sh
@@ -6,7 +6,3 @@ echo -e "\n1. Get all routes GET /routes\n"
 ALL_ROUTES_RESPONSE=$(curl -X GET http://localhost:8888/routesApi/routes)
 
 echo -e "Response:\n$ALL_ROUTES_RESPONSE\n"
-
-ALL_ROUTES_RESPONSE_OG=$(curl -X GET http://localhost:3000/routes)
-
-echo -e "Original Service Response:\n$ALL_ROUTES_RESPONSE_OG\n"
